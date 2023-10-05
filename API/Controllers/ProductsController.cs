fc6620fc43122b4562949c542dc333c10f9b1d92
dzmitry-trotsky <dimaop62@gmail.com>
2023-10-05 13:21:47 +0300

    Initial Commit

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    //
     [ApiController]
     [Route("/api/[controller]")]
     public class ProductsController : ControllerBase
