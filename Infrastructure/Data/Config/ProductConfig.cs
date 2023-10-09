fd06912ca5e55651004f1b893a16cd5e267c1960
dzmitry-trotsky <dimaop62@gmail.com>
2023-10-09 11:02:07 +0300

    FIX: add Infrastructure & Core files changing for previous commit

---
+++ Infrastructure/Data/Config/ProductConfig.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(_ => _.Id).IsRequired();
+            builder.Property(_ => _.Name).IsRequired().HasMaxLength(100);
+            builder.Property(_ => _.Description).IsRequired().HasMaxLength(180);
+            builder.Property(_ => _.Price).HasColumnType("decimal(10,2)");
+            builder.Property(_ => _.ImageUrl).IsRequired();
+            builder.HasOne(_ => _.ProductBrand).WithMany()
+                   .HasForeignKey(_ => _.ProductBrandId);
+            builder.HasOne(_ => _.ProductType).WithMany()
+                   .HasForeignKey(_ => _.ProductTypeId);
+        }
+    }
+}
---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationContext _context;
+        public ProductRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(_ => _.ProductBrand)
+                .Include(_ => _.ProductType)
+                .FirstOrDefaultAsync(_ => _.Id == id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products
+                .Include(_ => _.ProductBrand)
+                .Include(_ => _.ProductType)
+                .ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
+        }
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await _context.ProductBrands.ToListAsync();
+        }
+    }
+}
